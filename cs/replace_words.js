04936b50f2bd2f89b03a463c86cbd7b3d3592c85
newtover <newtover@gmail.com>
2016-11-25 18:19:17 +0300

    another try that seems to work better

--- cs/replace_words.js
+++ cs/replace_words.js
@@ -1,23 +1,55 @@
-var TEXT_NODE = document.TEXT_NODE;
+const TEXT_NODE = document.TEXT_NODE;
 
 function replaceWordsInTextNodes(node){
     for (let child of node.childNodes){
         if (child.nodeType != TEXT_NODE) { continue; }
         let oldText = child.nodeValue;
-        let newText = oldText.replace(/психотерапевт/g, 'нарколог');
+        let newText = oldText.replace(/психотерапевт/ig, 'нАрКоЛоГ');
         if (newText != oldText) {
-            console.log(newText);
+            // console.log(oldText, newText);
             let newNode = document.createTextNode(newText);
-            node.parentNode.replaceChild(newNode, node);
+            node.replaceChild(newNode, child);
         }
     }
 }
 
+function addMutationObserver(target){
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length) {
+                for (let addedNode of mutation.addedNodes){
+                    let nodeToReplaceIn;
+                    if (addedNode.classList.contains('comment')){
+                        // we are in a comment container
+                        nodeToReplaceIn = addedNode.querySelector('.comment-text');
+                        // console.log(nodeToReplaceIn);
+                    }
+                    if (nodeToReplaceIn){
+                        replaceWordsInTextNodes(nodeToReplaceIn);
+                    }
+                }
+            }
+        });
+    });
+    var config = { childList: true };
+    observer.observe(target, config);
+}
+
 function walkThroughTexts(){
+    for (let node of document.querySelectorAll('.comments_div')){
+        // new comments and expanded comments
+        addMutationObserver(node);
+    }
+
     for (let node of document.querySelectorAll('.text, .comment-text')){
+       // posts and comments
        replaceWordsInTextNodes(node);
-       console.log(node);
+//       console.log(node);
     } 
 }
-walkThroughTexts();
+
+// it' better to find a container for react, observe 
+// when it is first rendered, and then invoke walkThroughTexts(),
+// but I will not
+setTimeout(walkThroughTexts, 2000)
 
